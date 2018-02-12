8f47e13f62158592be862a12e732f997625daf29
George  Pianka <gpianka1@gmail.com>
2018-02-12 13:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function takeANumber(katzDeliLine, name) {
   let position = katzDeliLine.indexOf(name) +1;
   return `Welcome, ${name}. You are number ${position} in line.`;
 }
+function nowServing()
