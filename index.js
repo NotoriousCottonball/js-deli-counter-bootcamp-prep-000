39a644d5ed33742033bd7dacae75599645a45585
George  Pianka <gpianka1@gmail.com>
2018-02-12 14:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 }
   function currentLine (katzDeliLine) {
     if (katzDeliLine.length ===0) {
-      return "The line is currently empty empty.";
+      return "The line is currently empty.";
     } else {
        let line = "The line is currently: " ;
        for (let i=0; i<katzDeliLine.length; i++) {
