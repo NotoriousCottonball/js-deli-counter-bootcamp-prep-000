a4df9b5dbc924ca9360203a8dc7b2ba1adff6fd2
George  Pianka <gpianka1@gmail.com>
2018-02-12 13:44:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   } else {
     let person = katzDeliLine[0];
     katzDeliLine.shift();
-    return person;
+    return `Currently serving ${person}`;
   }
   
 }
